32127c7f3909c65d4257de0cff118ca1e5575dfc
sachinv26 <vermas7008@gmail.com>
2023-02-23 00:15:04 +0530

    first commit

---
+++ assets/projects.js
@@ -0,0 +1,47 @@
+export default [
+  {
+    title: "Sharkfolio",
+    description:
+      "A portfolio website of sharks. During the MLH hackathon, it was built with React, Tailwind CSS, and GSAP.",
+    image:
+      "https://user-images.githubusercontent.com/54346106/193443537-deab1b57-450d-4a1b-885e-0bb417b09882.png",
+    github: "https://github.com/rakeshsangem/harkfolio",
+    live: "https://sharkfolio.vercel.app/",
+  },
+  {
+    title: "DevArena",
+    description:
+      "DevArena is an online platform where users can interact with other developers, view their public repositories.",
+    image:
+      "https://user-images.githubusercontent.com/63954124/128591365-3ce2dc6c-5405-45b2-9f76-ac2b035899eb.png",
+    github: "https://github.com/sachinv26/DevArena",
+    live: "https://developersarena.netlify.app/",
+  },
+  {
+    title: "Portfolio",
+    description:
+      "A Ecommerce Website buit using HTML, CSS, and JavaScript. It has animations all over the watches and I made it during a hackathon",
+    image:
+      "https://user-images.githubusercontent.com/76443132/214965885-3c4b0f21-59e0-4ad4-90eb-93b14a2254a3.png",
+    github: "https://github.com/rakeshsangem/portflio",
+    live: "https://www.rakeshsangem.me",
+  },
+  {
+    title: "Portfolio",
+    description:
+      "A portflio website of me buit using HTML, CSS, and JavaScript. I made to opnesource during hacktoberfest",
+    image:
+      "https://user-images.githubusercontent.com/107752425/192083103-e773c142-6ed0-4223-8fca-62525c2f908a.png",
+    github: "https://github.com/rakeshsangem/portflio",
+    live: "https://www.rakeshsangem.me",
+  },
+  {
+    title: "Portfolio",
+    description:
+      "A portflio website of me buit using HTML, CSS, and JavaScript. I made to opnesource during hacktoberfest",
+    image:
+      "https://user-images.githubusercontent.com/107752425/192083103-e773c142-6ed0-4223-8fca-62525c2f908a.png",
+    github: "https://github.com/rakeshsangem/portflio",
+    live: "https://www.rakeshsangem.me",
+  },
+];
